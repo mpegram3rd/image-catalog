4626ef59620bc86166f02685c1f8c8b117cfd866
mpegram3rd <mpergam3rd@hotmail.com>
2025-11-19 07:50:13 -0500

    📝 Fixing some documentation

--- client_provider.py
+++ client_provider.py
@@ -8,6 +8,17 @@ model_url=os.environ['MODEL_URL']
 model_api_key=os.getenv('MODEL_API_KEY', '')
 
 def get_client():
+    """
+    Creates and returns an instance of the `OpenAI` compatible client configured with the
+    specified base URL and API key.
+
+    The function retrieves configuration details such as the model provider,
+    URL, and API key to initialize the `OpenAI` client instance.
+
+    :return: An initialized `OpenAI` compatible client instance configured with the
+        base URL and API key.
+    :rtype: OpenAI
+    """
     print(f"Using model provider: {model_provider}")
     return OpenAI(
         base_url=model_url,
@@ -15,6 +26,17 @@ def get_client():
     )
 
 def get_async_client():
+    """
+    Creates and returns an instance of the `AsyncOpenAI` compatible client configured with the
+    specified base URL and API key.
+
+    The function retrieves configuration details such as the model provider,
+    URL, and API key to initialize the `OpenAI` client instance.
+
+    :return: An initialized `AsyncOpenAI` compatible client instance configured with the
+        base URL and API key.
+    :rtype: AsyncOpenAI
+    """
     print(f"Using model provider: {model_provider}")
     return AsyncOpenAI(
         base_url=model_url,
