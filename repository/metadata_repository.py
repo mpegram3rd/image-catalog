73f4161eb263541c10d534feed1e6bb7c41a1746
mpegram3rd <mpergam3rd@hotmail.com>
2025-12-15 06:32:22 -0500

    🚧 Playing with best way to vectorize image details extracted from AI.

--- repository/metadata_repository.py
+++ repository/metadata_repository.py
@@ -1,6 +1,5 @@
 import chromadb
 import chromadb.utils.embedding_functions as embedding_functions
-from openai.types import CreateEmbeddingResponse
 
 from ai.client_provider import get_client
 from config import Config
@@ -8,7 +7,7 @@ from models.models import AnalysisResult, Metadata
 
 config = Config('.env')
 
-dbclient = chromadb.PersistentClient(path="./data/metadata.db")
+dbclient = chromadb.PersistentClient(path="./data/image_data.db")
 
 embedding_func = embedding_functions.OpenAIEmbeddingFunction(
                 api_key=config.llm_api_key,
@@ -22,6 +21,17 @@ metadata_collection = dbclient.get_or_create_collection(
     name="metadata",
     embedding_function=embedding_func,
     configuration = {
+        "hnsw": {
+            "space": "cosine",
+            "ef_construction": 200
+        }
+    }
+)
+
+description_collection = dbclient.get_or_create_collection(
+    name="descriptions",
+    embedding_function=embedding_func,
+    configuration={
         "hnsw": {
             "space": "cosine",
             "ef_construction": 200
@@ -47,38 +57,32 @@ def add_analysis(image_path: str, data: AnalysisResult):
 
     # metadata_str = metadata.model_dump_json()
 
-    metadata_collection.add (
+    description_collection.add (
         ids=[image_path],
         documents=[data.description],
         metadatas=[metadata.model_dump()
-            # metadata.model_dump_json
-         #    {
-         #    "tags": ["python", "vector-db", "chromadb"],
-         #    "scores": [0.5, 0.8, 0.9],
-         #    "misc": {"nested": ["a", "b", "c"]}  # nested structures also allowed
-         # }
         ]
     )
 
+    metadata_collection.add(
+        ids=[image_path],
+        documents=[data.model_dump_json()]
+    )
+
 def list_contents():
-    batch= metadata_collection.get(
+    batch= description_collection.get(
         include=["metadatas", "documents"]
     )
     print(batch)
 
-    result = metadata_collection.query(
-        include=["metadatas", "documents"],
+    result = description_collection.query(
+        include=["metadatas", "documents", "distances"],
         query_texts=["Fall leaves"]
     )
     print (result)
 
     result2 = metadata_collection.query(
-        include=["metadatas", "documents"],
-        query_texts=[""],
-        where={
-            "colors": {
-                "$eq": "red"
-            }
-        }
+        include=["metadatas", "documents", "distances"],
+        query_texts=["\"color\":\"brown\""]
     )
     print (result2)
