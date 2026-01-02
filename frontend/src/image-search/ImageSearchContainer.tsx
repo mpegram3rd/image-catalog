ffa41d00566245903e64176cf193025b5c13737d
Macon Pegram <mpegram@captechconsulting.com>
2026-01-02 16:06:53 -0500

    ♻️ Refactored UI to be a bit more like what I'd like it to look like.

--- frontend/src/image-search/ImageSearchContainer.tsx
+++ frontend/src/image-search/ImageSearchContainer.tsx
@@ -1,12 +1,8 @@
-import TextSearchForm from "./text-search/TextSearchForm.tsx";
 import ImageDropper from "./image-match/ImageDropper.tsx";
 
 export default function ImageSearchContainer () {
 
     return (
-        <>
-            <ImageDropper/>
-            <TextSearchForm />
-        </>
+        <ImageDropper/>
     );
 }
