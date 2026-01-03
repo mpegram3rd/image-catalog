e624b226a928456ac75801390503d040b9c82bc3
Macon Pegram <mpegram@captechconsulting.com>
2026-01-03 17:57:24 -0500

    ✨ Stubbed Carousel implementation

--- frontend/src/image-results/ImageResultsContainer.tsx
+++ frontend/src/image-results/ImageResultsContainer.tsx
@@ -1,9 +1,22 @@
+import {Carousel} from "@mantine/carousel";
+
+import '@mantine/core/styles.css';
+import '@mantine/carousel/styles.css';
 import styles from './ImageResultsContainer.module.css'
 
 export default function ImageResultsContainer () {
     return (
-        <span className={styles.container}>
-            Hello World!
-        </span>
+        <Carousel
+            height={200}
+            className={styles.container}
+            withIndicators={true}
+            emblaOptions={{
+                align: 'center'
+            }}
+        >
+            <Carousel.Slide>1</Carousel.Slide>
+            <Carousel.Slide>2</Carousel.Slide>
+            <Carousel.Slide>3</Carousel.Slide>
+        </Carousel>
     );
 }
