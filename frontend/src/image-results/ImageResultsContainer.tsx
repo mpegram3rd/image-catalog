8e88e20f41c924ff312db23f1467452375081c73
Macon Pegram <mpegram@captechconsulting.com>
2026-01-03 19:35:03 -0500

    ✨ Props + Carousel are connected.  Images not working and more styling needed.

--- frontend/src/image-results/ImageResultsContainer.tsx
+++ frontend/src/image-results/ImageResultsContainer.tsx
@@ -1,10 +1,12 @@
 import {Carousel} from "@mantine/carousel";
 
+import type {ImageMatchResult, ImageSearchResults} from "../models/ImageSearchResults.ts";
+import * as React from "react";
+import {Image} from "@mantine/core";
+
 import '@mantine/core/styles.css';
 import '@mantine/carousel/styles.css';
 import styles from './ImageResultsContainer.module.css'
-import type {ImageMatchResult, ImageSearchResults} from "../models/ImageSearchResults.ts";
-import * as React from "react";
 
 const ImageResultsContainer: React.FC<ImageSearchResults> = ({ results }) => {
     return (
@@ -12,14 +14,21 @@ const ImageResultsContainer: React.FC<ImageSearchResults> = ({ results }) => {
             height={200}
             className={styles.container}
             withIndicators={true}
+            slideSize={"80%"}
+            classNames={{slide: styles.slides}}
             emblaOptions={{
+                loop: true,
+                dragFree: false,
                 align: 'center'
             }}
         >
             {results.map((result: ImageMatchResult) => (
                     <Carousel.Slide>
-                        <div><b>Image:</b> {result.image_path}</div>
-                        <div><b>Description:</b> {result.description}</div>
+                        <Image
+                            radius='md'
+                            src={result.image_path}
+                        />
+                        <div>{result.description}</div>
                     </Carousel.Slide>
                 ))
             }
