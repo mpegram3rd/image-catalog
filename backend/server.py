607d8a32548aaba50a6b0a805a4fe90a761aefd2
mpegram3rd <mpergam3rd@hotmail.com>
2026-03-23 07:06:05 -0400

    ⬆️ Fix uvicorn warning for deprecated websockets

--- backend/server.py
+++ backend/server.py
@@ -39,4 +39,4 @@ async def setup():
 
 if __name__ == "__main__":
     asyncio.run(setup())
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000, ws="websockets-sansio")
